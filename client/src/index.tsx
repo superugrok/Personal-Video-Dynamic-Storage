e3e81234d1fc3c1e5ef5fbd2b912606148e3b8e6
erwin <ciberteamserver@gmail.com>
2022-10-30 03:21:55 +0300

    title

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "@App/App";
 
+document.title = "Aligned assigment";
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(<App />);
